61616ce09955bb68bb22d411bedc122feb115bf8
leobaiao <leobaiao966@gmail.com>
2025-01-11 15:19:17 -0300

    adicionada linha com versão da aplicação

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine(DateTime.Now.ToLongDateString());
+Console.WriteLine("Version Aplicaction 1.0.0");
